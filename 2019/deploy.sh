f87c0bd0dbaa28048d06ae1df2f9c465fb5ecb1c
Morton Swimmer <morton_swimmer@trendmicro.com>
2019-05-03 17:47:37 +0200

    copying the beta site to the main 2019 site

---
+++ 2019/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AWS_OPTS="--profile personal.iam --region eu-west-1"
+BUCKET="2019.elbsides.de"
+CERT_ARN="arn:aws:acm:eu-west-1:006815045256:certificate/d3016d8a-8498-47cf-8c2a-ab901024c48c"
+
+bundle update
+
+# Better to set this up manually together with all the Route53 stuff
+#aws $AWS_OPTS s3 mb s3://$BUCKET
+
+aws $AWS_OPTS resourcegroupstaggingapi tag-resources --tags Project=Elbsides --resource-arn-list arn:aws:s3:::2019.elbsides.de
+
+aws $AWS_OPTS s3api put-bucket-policy --bucket $BUCKET --policy file://s3_bucket_permissions.json
+
+bundle exec jekyll build
+
+aws $AWS_OPTS s3 sync _site/ s3://$BUCKET/ --delete --exclude ".DS_Store" 
+
+aws $AWS_OPTS s3 website s3://$BUCKET \
+    --index-document index.html \
+    --error-document 404.html
---
+++ 2019/registration.md
@@ -0,0 +1,8 @@
+---
+title: "Registration"
+layout: archive
+permalink: /registration
+
+---
+
+Registration for Elbsides 2019    isn't open yet. Stay tuned by following us on Twitter at: [@Elbsides](https://twitter.com/elbsides).
